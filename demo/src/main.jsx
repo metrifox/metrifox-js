8ac777af16cc29eba1f7062831c59feecec176e9
Ibrahim Nayaya <ibrahimnayaya25@gmail.com>
2025-08-29 11:48:13 +0100

    cleanup

--- demo/src/main.jsx
+++ demo/src/main.jsx
@@ -4,7 +4,7 @@ import { init } from 'metrifox-js'
 import App from './App.jsx'
 
 // Initialize Metrifox SDK once at app startup
-init({ apiKey: "3d2475a57807243ab853bc0250d8eb30608074da355ca321394ae71a2aa882b7", webAppBaseUrl: "http://localhost:3000", baseUrl: "http://localhost:3003/api/v1/" });
+init({ apiKey: import.meta.env.VITE_METRIFOX_API_KEY, webAppBaseUrl: "http://localhost:3000", baseUrl: "http://localhost:5000/api/v1/" });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
